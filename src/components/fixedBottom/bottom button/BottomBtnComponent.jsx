58a49eaacbba8894271e501171ccc221687a049e
azerBoughorra <azer.boughorra@gmail.com>
2022-03-06 12:08:43 +0100

    bottom buttons animation

--- src/components/fixedBottom/bottom button/BottomBtnComponent.jsx
+++ src/components/fixedBottom/bottom button/BottomBtnComponent.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
+import { noiseTrigger } from "../../../hooks/useTrigger";
 import "./BottomBtnStyle.scss";
 
 function BottomBtn({ text }) {
+  const textRef1 = useRef();
+  const textRef2 = useRef();
+  const textRef3 = useRef();
+
   return (
-    <div className="bottom_btn">
-      <span className="bottom_button_text">{text}</span>
+    <div
+      className="bottom_btn"
+      onMouseEnter={() =>
+        noiseTrigger(textRef1.current, textRef2.current, textRef3.current)
+      }
+    >
+      <span className="bottom_button_text" ref={textRef1}>
+        {text}
+      </span>
+      <span className="bottom_icons" ref={textRef2}>
+        {text}
+      </span>
+      <span className="bottom_icons" ref={textRef3}>
+        {text}
+      </span>
+
       <span className="brackets_bottom bracket_left_bottom">[</span>
       <span className="brackets_bottom bracket_right_bottom">]</span>
     </div>
